e6a727a6edfe555afe40a10fa328bffdeeee982a
neiro <neiro.mail@gmail.com>
2015-04-15 20:23:42 +0300

    Use rails-assets with jekyll-assets properly

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,11 +1,12 @@
 require 'jekyll-assets'
 require 'jekyll-assets/compass'
+require 'susy'
 require 'bundler/setup'
 
 Bundler.require(:default, 'development')
 
 if defined?(RailsAssets)
   RailsAssets.load_paths.each do |path|
-    settings.sprockets.append_path(path)
+    Sprockets.append_path path
   end
 end
